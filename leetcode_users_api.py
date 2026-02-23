ff0daca170180aa23d420abe0f7aa67b7a8bc2e8
jatin <jg9257694@gmail.com>
2026-02-23 13:04:00 +0530

    modifying the request for multiple users

--- leetcode_users_api.py
+++ leetcode_users_api.py
@@ -7,42 +7,46 @@ app = FastAPI()
 @app.get("/leetcode/user/{username}")
 def fetch_data(username: str):
     url = "https://leetcode.com/graphql/"
-    payload = {
+    
+
+    userList : str = username.split(',')
+    output = {}
+    for user in userList:
+        payload = {
         "query": """
-        query userSessionProgress($username: String!) {
-          allQuestionsCount {
-            difficulty
-            count
-          }
-          matchedUser(username: $username) {
-            submitStats {
-              acSubmissionNum {
+            query userSessionProgress($username: String!) {
+              allQuestionsCount {
                 difficulty
                 count
-                submissions
               }
-              totalSubmissionNum {
-                difficulty
-                count
-                submissions
+              matchedUser(username: $username) {
+                submitStats {
+                  acSubmissionNum {
+                    difficulty
+                    count
+                    submissions
+                  }
+                  totalSubmissionNum {
+                    difficulty
+                    count
+                    submissions
+                  }
+                }
               }
             }
-          }
+            """,
+            "variables": {"username": user},
+            "operationName": "userSessionProgress"
         }
-        """,
-        "variables": {"username": username},
-        "operationName": "userSessionProgress"
-    }
+        response = requests.post(url, json = payload, headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Content-Type": "application/json"
+          }
+        )
 
-    response = requests.post(url, json=payload, headers={
-          "User-Agent": "Mozilla/5.0",
-          "Content-Type": "application/json"
-      }
-    )
-        
-    try:
-        data = response.json()  # parse JSON response
-    except Exception:
-        data = {"error": "Failed to parse response", "raw": response.text}
-
-    return JSONResponse(content=data, status_code=response.status_code)
+        try:
+            data = response.json()
+        except Exception:
+            data = {"error": "Failed to parse response", "raw": response.text}
+        output[user] = data
+    return JSONResponse(content = output)
